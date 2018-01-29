f9023d82acc8763019bc26982defb2b8e1913fcb
Villermen <villermen@gmail.com>
2018-01-29 21:36:39 +0100

    Part of player lookup page

--- src/Controller/LookupController.php
+++ src/Controller/LookupController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\DailyRecord;
+use App\Entity\TrackedHighScore;
 use App\Entity\TrackedPlayer;
 use App\Service\TimeKeeper;
 use DateTime;
@@ -11,6 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Villermen\RuneScape\HighScore\HighScoreSkill;
+use Villermen\RuneScape\HighScore\HighScoreSkillComparison;
+use Villermen\RuneScape\Player;
+use Villermen\RuneScape\RuneScapeException;
 
 /**
  * @Route("/", name="app_")
@@ -31,15 +36,22 @@ class LookupController extends Controller
         $name1 = $request->query->get("player1");
         $name2 = $request->query->get("player2");
 
-        if ($name1 && $name2) {
-            return $this->redirectToRoute("app_lookup_compare", [
-                "name1" => $name1,
-                "name2" => $name2
-            ]);
-        } elseif ($name1) {
-            return $this->redirectToRoute("app_lookup_player", [
+        if ($name1) {
+            // Remove name query parameters but pass on the others
+            $query = array_filter($request->query->all(), function($parameter) {
+                return !in_array($parameter, ["player1", "player2"]);
+            }, ARRAY_FILTER_USE_KEY);
+
+            if ($name2) {
+                return $this->redirectToRoute("app_lookup_compare", array_merge([
+                    "name1" => $name1,
+                    "name2" => $name2
+                ], $query));
+            }
+
+            return $this->redirectToRoute("app_lookup_player", array_merge([
                 "name" => $name1
-            ]);
+            ], $query));
         }
 
         $dailyRecords = $entityManager->getRepository(DailyRecord::class)->findBy(
@@ -63,13 +75,61 @@ class LookupController extends Controller
 
     /**
      * @param string $name
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @param TimeKeeper $timeKeeper
      * @return Response
      *
      * @Route("/{name}", name="lookup_player")
      */
-    public function playerAction(string $name)
+    public function playerAction(string $name, EntityManagerInterface $entityManager, Request $request,
+        TimeKeeper $timeKeeper)
     {
-        return new Response("lookup " . htmlspecialchars($name));
+        $error = "";
+        $stats = false;
+        $trainedToday = false;
+        $trainedYesterday = false;
+        $trainedWeek = false;
+
+        $player = $entityManager->getRepository(TrackedPlayer::class)->findOneBy(["name" => $name]);
+
+        // Try to obtain a tracked player from the database
+        if (!$player) {
+            try {
+                $player = new Player($name);
+            } catch (RuneScapeException $exception) {
+                $error = "Invalid player name requested.";
+            }
+        }
+
+        if ($player) {
+            try {
+                // Fetch live stats
+                $stats = $player->getHighScore($request->query->has("oldschool"));
+            } catch (RuneScapeException $exception) {
+                $error = "Could not fetch player stats.";
+            }
+        }
+
+        if ($player && $player instanceof TrackedPlayer) {
+            // Fetch and compare tracked stats
+            $trackedHighScoreRepository = $entityManager->getRepository(TrackedHighScore::class);
+
+            $highScoreToday = $trackedHighScoreRepository->findOneBy(["date" => $timeKeeper->getUpdateTime(0)]);
+            if ($highScoreToday) {
+                // TODO: Full HighScore comparison instead of per skill?
+            }
+        }
+
+        return $this->render("lookup/player.html.twig", [
+            "error" => $error,
+            "player" => $player,
+            "stats" => $stats,
+            "tracked" => $player instanceof TrackedPlayer,
+            "trainedToday" => $trainedToday,
+            "trainedYesterday" => $trainedYesterday,
+            "trainedWeek" => $trainedWeek
+        ]);
     }
 
     /**
