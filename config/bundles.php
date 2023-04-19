80fe261d84da818354bc602d709656117811cf91
Villermen <villermen@gmail.com>
2023-04-19 22:04:52 +0200

    Upgrade symfony/flex to support PHP 8.2

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
