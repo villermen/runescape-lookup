a2fca8c23edc94f5ea2fe9f4d27b5f661e83c77f
Villermen <villermen@gmail.com>
2023-04-19 21:46:07 +0200

    Unfinished work from long ago

--- src/frontend/actions/index.ts
+++ src/frontend/actions/index.ts
@@ -1,7 +1,41 @@
 import { AppDispatch, AppState } from '../store';
+import { PlayerData } from '../types';
 
-export function setPlayers(playerName1: string|null, playerName2: string|null) {
-    return (dispatch: AppDispatch, state: AppState) => {
-        console.log('setPlayers');
+export const SET_LOADING = 'SET_LOADING';
+export const SET_PLAYER_NAMES = 'SET_PLAYER_NAMES';
+export const SET_PLAYER_DATA = 'SET_PLAYER_DATA';
+
+const samplePlayerData = {
+    name: 'Villermen',
+    skills: [],
+    activities: [],
+    log: [],
+};
+
+// TODO: Make this into a non-redux method that just triggers all the redux actions?
+export function setPlayers(player1Name: string|null, player2Name: string|null) {
+    return async (dispatch: AppDispatch, state: AppState) => {
+        dispatch({
+            type: SET_PLAYER_NAMES,
+            payload: [player1Name, player2Name],
+        }); // TODO: Triggers URL change via react-router (useHistory())
+        dispatch({
+            type: SET_LOADING,
+            payload: true,
+        });
+
+        const fetchPlayerData = async (name: string|null): Promise<PlayerData|null> => {
+            return name ? samplePlayerData : null;
+        };
+
+        const playerData = await Promise.all([
+            fetchPlayerData(player1Name),
+            fetchPlayerData(player2Name)
+        ]);
+
+        dispatch({
+            type: SET_PLAYER_DATA,
+            payload: playerData,
+        })
     };
 }
--- src/frontend/types.ts
+++ src/frontend/types.ts
@@ -1,5 +1,33 @@
 export interface PlayerData {
-    stats: null,
-    activites: null,
-    log: null,
+    name: string,
+    skills: HighscoreSkill[],
+    activities: HighscoreActivity[],
+    log: LogItem[],
+    // tracked: boolean;
+}
+
+export interface HighscoreSkill {
+    id: number;
+    name: string;
+    xp: number;
+    rank: number;
+    level: number;
+    virtualLevel: number;
+    xpToNextLevel: number;
+    progressToNextLevel: number;
+    // highscore:
+    // highscoreDate:
+}
+
+export interface HighscoreActivity {
+    id: number;
+    name: string;
+    score: number;
+    rank: number;
+}
+
+export interface LogItem {
+    time: string;
+    title: string;
+    description: string;
 }
