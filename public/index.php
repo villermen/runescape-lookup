3654092a0a2affc4e3300165b33fca6f7b40ef97
Villermen <villermen@gmail.com>
2025-03-14 09:25:09 +0100

    Use simpler table sorting library

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
 use App\Kernel;
 
 // Let dev-server handle regular files.
-if (php_sapi_name() === 'cli-server' && is_file(sprintf('%s/%s', __DIR__, $_SERVER['REQUEST_URI']))) {
-    return false;
+if (php_sapi_name() === 'cli-server') {
+    if (preg_match('~^(/[^#?]+)~', $_SERVER['REQUEST_URI'], $matches)) {
+        if (is_file(__DIR__ . $matches[1])) {
+            return false;
+        }
+    }
 }
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
